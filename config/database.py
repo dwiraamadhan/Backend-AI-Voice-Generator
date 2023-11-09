e4b33bb7de79a0d2a7cbf0e629bccc634203a655
dwiraamadhan <dwiramadhan50@gmail.com>
2023-11-09 14:41:31 +0700

    delete "menghapus file yang sudah tidak terpakai"

--- config/database.py
+++ config/database.py
@@ -2,8 +2,7 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["audio_ml"]
-collection_name = db["audio_file"]
+collection_audio = db["audio_file"]
 collection_text = db["text_file"]
-collection_knowledge = db["knowledge_file"]
 collection_questions = db["question"]
-collection_knowledge_for_pinecone = db["knowledge_pinecone"]
+collection_knowledge = db["knowledge_file"]
