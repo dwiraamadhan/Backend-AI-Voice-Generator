0aceefb6132ee8871e79435117f3334ae0b80032
dwiraamadhan <dwiramadhan50@gmail.com>
2023-11-16 16:27:05 +0700

    refactor:mengganti vector db menjadi PGVector

--- api/add_knowledge.py
+++ api/add_knowledge.py
@@ -1,14 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from pymilvus import (
-    utility,
-    connections,
-)
 from docx import Document
 from langchain.text_splitter import (
     RecursiveCharacterTextSplitter,
 )
 from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.vectorstores import Milvus, ElasticsearchStore
 import os
 from langchain.vectorstores.pgvector import PGVector
 
@@ -20,75 +15,45 @@ router = APIRouter()
 async def add_knowledge(
     knowledge_file: UploadFile = File(...),
 ):
-    # read the document
-    document = Document(knowledge_file.file)
-    knowledge_text = ""
-    for paragraph in document.paragraphs:
-        knowledge_text += paragraph.text + " "
-    print(knowledge_text)
+    try:
+        # read the document
+        document = Document(knowledge_file.file)
+        knowledge_text = ""
+        for paragraph in document.paragraphs:
+            knowledge_text += paragraph.text + " "
+        print(knowledge_text)
 
-    # split the text into chunks
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200, length_function=len
-    )
-    print("file sudah di pecah menjadi beberapa text")
+        # split the text into chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, length_function=len
+        )
+        print("file sudah di pecah menjadi beberapa text")
 
-    # convert the chunks into documents
-    docs = text_splitter.create_documents([knowledge_text])
-    print("text pecahan sudah dibuat menjadi dokumen")
+        # convert the chunks into documents
+        docs = text_splitter.create_documents([knowledge_text])
+        print("text pecahan sudah dibuat menjadi dokumen")
 
-    # print(texts)
-    print(len(docs))
+        # print(texts)
+        print(len(docs))
 
-    # load Embeddings model
-    embeddings = SentenceTransformerEmbeddings(model_name=os.getenv("EMBEDDING_MODEL"))
-    print("embeddings sudah di load")
+        # load Embeddings model
+        embeddings = SentenceTransformerEmbeddings(
+            model_name=os.getenv("EMBEDDING_MODEL")
+        )
+        print("embeddings sudah di load")
 
-    # # create connection to milvus to check existed collection
-    # milvus_connect = connections.connect(
-    #     host=os.getenv("MILVUS_HOST"), port=os.getenv("MILVUS_PORT")
-    # )
-    # if utility.has_collection(os.getenv("MILVUS_COLLECTION")):
-    #     utility.drop_collection(os.getenv("MILVUS_COLLECTION"))
+        db = PGVector.from_documents(
+            embedding=embeddings,
+            documents=docs,
+            collection_name=os.getenv("COLLECTION_NAME"),
+            connection_string=os.getenv("CONNECTION_STRING"),
+        )
 
-    # # save vector data to database Milvus
-    # vector_store = Milvus.from_documents(
-    #     docs,
-    #     embedding=embeddings,
-    #     collection_name=os.getenv("MILVUS_COLLECTION"),
-    #     connection_args={
-    #         "host": os.getenv("MILVUS_HOST"),
-    #         "port": os.getenv("MILVUS_PORT"),
-    #     },
-    # )
+        print("vector store PGVector sudah di load")
 
-    # vector_store = ElasticsearchStore(
-    #     embedding=embeddings,
-    #     index_name="chatbotBNIDirect",
-    #     es_user="magang",
-    #     es_password="magang12345",
-    #     es_url="http://154.41.251.22:9200",
-    # )
+        return {
+            "message": "knowledge has been added successfully",
+        }
 
-    # CONNECTION_STRING = PGVector.connection_string_from_db_params(
-    #     driver=os.getenv("PGVECTOR_DRIVER"),
-    #     host=os.getenv("PGVECTOR_HOST"),
-    #     port=os.getenv("PGVECTOR_PORT"),
-    #     database=os.getenv("PGVECTOR_DATABASE"),
-    #     user=os.getenv("PGVECTOR_USER"),
-    #     password=os.getenv("PGVECTOR_PASSWORD"),
-    # )
-
-    COLLECTION_NAME = "chatbotBNIDirect"
-    db = PGVector.from_documents(
-        embedding=embeddings,
-        documents=docs,
-        collection_name=COLLECTION_NAME,
-        connection_string=os.getenv("CONNECTION_STRING"),
-    )
-
-    print("vector store milvus sudah di load")
-
-    return {
-        "message": "knowledge has been added successfully",
-    }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=e.args[0])
--- functions/question_answering.py
+++ functions/question_answering.py
@@ -2,11 +2,9 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
 import torch
 from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.vectorstores import Milvus, ElasticsearchStore
 from langchain.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 import os
-from elasticsearch import Elasticsearch
 from langchain.vectorstores.pgvector import PGVector
 
 # load model question answering
@@ -35,32 +33,8 @@ def llm_pipeline():
 def qa_llm():
     llm = llm_pipeline()
     embeddings = SentenceTransformerEmbeddings(model_name=os.getenv("EMBEDDING_MODEL"))
-
-    # initialize vector store Milvus
-    # db = Milvus(
-    #     embeddings,
-    #     collection_name=os.getenv("MILVUS_COLLECTION"),
-    #     connection_args={
-    #         "host": os.getenv("MILVUS_HOST"),
-    #         "port": os.getenv("MILVUS_PORT"),
-    #     },
-    # )
-
-    # es_connection = Elasticsearch(
-    #     "http://154.41.251.22:9200",
-    #     bearer_auth="UWt5cnpJc0JLNndiZXBhYlhHOTk6Ul9jVEJzenlUQy1WVURfUy1PMHBEQQ==",
-    #     # basic_auth=("magang", "magang12345"),
-    # )
-
-    # db = ElasticsearchStore(
-    #     embedding=embeddings,
-    #     index_name="chatbotBNIDirect",
-    #     es_connection=es_connection,
-    # )
-
-    COLLECTION_NAME = "chatbotBNIDirect"
     db = PGVector(
-        collection_name=COLLECTION_NAME,
+        collection_name=os.getenv("COLLECTION_NAME"),
         connection_string=os.getenv("CONNECTION_STRING"),
         embedding_function=embeddings,
     )
